9d8fc35cbb9b57b4c9b13dc0411451756421c063
animevn <haanhth21@gmail.com>
2020-04-03 16:15:44 +0700

    change to mvc

--- Weather/controller/AppDelegate.swift
+++ Weather/controller/AppDelegate.swift
@@ -8,34 +8,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions:[UIApplication.LaunchOptionsKey:Any]?)->Bool{
-        
+
         window = UIWindow(frame: UIScreen.main.bounds)
-        let weather = WeatherController()
-        window?.rootViewController = weather
-        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        
+   func applicationDidBecomeActive(_ application: UIApplication) {
+        print("app did become active")
     }
-
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+       print("app will become inactive")
+    }
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
-        
+        print("app did enter background")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-
+       print("app will become active")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-
+        print("app did terminate")
     }
-
 
 }
 
