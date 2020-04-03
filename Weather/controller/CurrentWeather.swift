86ab8477fb8adbe97f599656dc232888958034ab
animevn <haanhth21@gmail.com>
2020-04-03 23:01:00 +0700

    test git (Xcode git control is low level vs Visual Code or Idea

--- Weather/controller/CurrentWeather.swift
+++ Weather/controller/CurrentWeather.swift
@@ -3,10 +3,16 @@ import UIKit
 class CurrentWeather:UIView{
     
     let height:CGFloat = screen().y * 3.5 / 10 - Constants.inset * 2
+    private var lbTemp = UILabel()
+    private var lbMinMax = UILabel()
+    private var lbCity = UILabel()
+    private var lbWeather = UILabel()
+    private var ivWeather = UIImageView()
     
     deinit {
         print("The class \(type(of: self)) was remove from memory")
     }
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
